5f51125e11e3d155b67b22ec1ca50a3e41c754f0
willianfurtado <willianjj15@hotmail.com>
2025-11-01 18:10:46 -0300

    Finishing login screen

--- lib/login.dart
+++ lib/login.dart
@@ -10,6 +10,7 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   String email = '';
   String senha = '';
+  bool _isPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
@@ -42,8 +43,169 @@ class _LoginState extends State<Login> {
                 ),
 
                 //Campo de email
-
+                const Text(
+                  'Email',
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+                ),
+                const SizedBox(height: 8),
+                TextField(
+                  onChanged: (text) {
+                    email = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    hintText:
+                        'Digite seu email...', // Placeholder como no protótipo
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 15.0,
+                      horizontal: 10.0,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      borderSide: const BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      borderSide: const BorderSide(
+                        color: Colors.green,
+                        width: 2.0,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 25),
                 //Campo de senha
+                const Text(
+                  'Senha',
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+                ),
+                TextField(
+                  onChanged: (text) {
+                    senha = text;
+                  },
+                  obscureText: !_isPasswordVisible,
+                  decoration: InputDecoration(
+                    hintText: 'Digite sua senha...',
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 15.0,
+                      horizontal: 10.0,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      borderSide: const BorderSide(color: Colors.green),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      borderSide: const BorderSide(
+                        color: Colors.grey,
+                        width: 2.0,
+                      ),
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: Colors.black,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: TextButton(
+                    onPressed: () {
+                      //Implementar recuperação de senha
+                    },
+                    child: Text(
+                      'Esqueceu sua Senha?',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                //Botão de entrar
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      //Implementar lógica de login
+                      print('Email: $email, Senha: $senha');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.lightBlue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      elevation: 0,
+                    ),
+                    child: const Text(
+                      'Entrar',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 30),
+
+                Row(
+                  children: [
+                    Expanded(child: Divider(color: Colors.grey)),
+                    Padding(
+                      padding: EdgeInsetsGeometry.symmetric(horizontal: 10.0),
+                      child: Text('ou', style: TextStyle(color: Colors.blue)),
+                    ),
+                    Expanded(child: Divider(color: Colors.grey)),
+                  ],
+                ),
+
+                // const SizedBox(height: 30),
+
+                // 7. Botão Entrar com o Google
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton.icon(
+                    onPressed: () {
+                      // Lógica de login com Google
+                    },
+                    style: OutlinedButton.styleFrom(
+                      foregroundColor: Colors.black87, // Cor do texto
+                      backgroundColor: Colors.white, // Fundo branco
+                      side: const BorderSide(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ), // Borda fina cinza
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          8.0,
+                        ), // Bordas arredondadas
+                      ),
+                    ),
+                    // icon: Image.asset(
+                    //   'assets/images/google-icon.svg',
+                    //   height: 20.0,
+                    // ),
+                    label: const Text(
+                      'Entrar com o Google',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
