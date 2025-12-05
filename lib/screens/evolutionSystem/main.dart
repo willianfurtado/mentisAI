0f13a2a00c481edcceb7b85a0b1dc5b920b05a7e
willianfurtado <willianjj15@hotmail.com>
2025-12-04 23:45:46 -0300

    refactor

--- lib/screens/evolutionSystem/main.dart
+++ lib/screens/evolutionSystem/main.dart
@@ -25,22 +25,6 @@ class EvolutionSystem extends StatelessWidget {
               ),
               const SizedBox(height: 20),
 
-              EvolutionLineChart(
-                dataSpots: const [
-                  FlSpot(0, 10.0),
-                  FlSpot(1, 17.5),
-                  FlSpot(2, 25.0),
-                ],
-                // Rótulos correspondentes ao eixo X
-                bottomLabels: const [
-                  '8 de jul.',
-                  '9 de jul.',
-                  '10 de jul.',
-                ],
-                // O valor mais alto na escala Y (25 mil)
-                maxYValue: 25.0,
-              ),
-
               GridView.count(
                 crossAxisCount: 2,
                 mainAxisSpacing: 20.0,
