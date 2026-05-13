ab2a9bfe51f428b512a503c2d753758f53dd4965
willianfurtado <willianjj15@hotmail.com>
2026-05-13 15:25:23 -0300

    fix: consertando bug do banco de dados

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -30,15 +30,15 @@ class DataBaseService {
   }
 
   _onCreate(db, version) async {
-    await db.execute('''
-      CREATE TABLE $tableNameMetrics (
-        id INTEGER PRIMARY KEY, 
-        date TEXT NOT NULL UNIQUE, 
-        steps INTEGER NOT NULL,
-        calories INTEGER NOT NULL,
-        sleepQuality REAL NOT NULL
-      )
-    ''');
+    // await db.execute('''
+    //   CREATE TABLE $tableNameMetrics (
+    //     id INTEGER PRIMARY KEY, 
+    //     date TEXT NOT NULL UNIQUE, 
+    //     steps INTEGER NOT NULL,
+    //     calories INTEGER NOT NULL,
+    //     sleepQuality REAL NOT NULL
+    //   )
+    // ''');
 
     await db.execute('''
       CREATE TABLE $tableNameUser (
@@ -66,9 +66,13 @@ class DataBaseService {
     );
   }
   
-  Future<UserData?> getUser() async {
+  Future<UserData?> getUser(int id) async {
     final db = await database;
-    final List<Map<String, dynamic>> maps = await db.query(tableNameUser);
+    final List<Map<String, dynamic>> maps = await db.query(
+      tableNameUser,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
 
     if (maps.isNotEmpty) {
       return UserData.fromMap(maps.first);
@@ -76,58 +80,58 @@ class DataBaseService {
     return null;
   }
 
-  Future<int> insertMetric(MetricData metric) async {
-    final db = await database;
-    return await db.insert(
-      tableNameMetrics,
-      metric.toMap(),
-      conflictAlgorithm: ConflictAlgorithm.replace,
-    );
-  }
+  // Future<int> insertMetric(MetricData metric) async {
+  //   final db = await database;
+  //   return await db.insert(
+  //     tableNameMetrics,
+  //     metric.toMap(),
+  //     conflictAlgorithm: ConflictAlgorithm.replace,
+  //   );
+  // }
 
-  Future<MetricData?> getMetricByDate(String date) async {
-    final db = await database;
-    final List<Map<String, dynamic>> maps = await db.query(
-      tableNameMetrics,
-      where: 'date = ?',
-      whereArgs: [date],
-    );
+  // Future<MetricData?> getMetricByDate(String date) async {
+  //   final db = await database;
+  //   final List<Map<String, dynamic>> maps = await db.query(
+  //     tableNameMetrics,
+  //     where: 'date = ?',
+  //     whereArgs: [date],
+  //   );
 
-    if (maps.isNotEmpty) {
-      return MetricData.fromMap(maps.first);
-    } else {
-      return null;
-    }
-  }
+  //   if (maps.isNotEmpty) {
+  //     return MetricData.fromMap(maps.first);
+  //   } else {
+  //     return null;
+  //   }
+  // }
 
-  Future<void> seedDatabase() async {
-    final db = await database;
+  // Future<void> seedDatabase() async {
+  //   final db = await database;
 
-    await db.delete(tableNameMetrics);
+  //   await db.delete(tableNameMetrics);
 
-    final now = DateTime.now().toUtc().copyWith(
-        hour: 0, minute: 0, second: 0, millisecond: 0, microsecond: 0);
+  //   final now = DateTime.now().toUtc().copyWith(
+  //       hour: 0, minute: 0, second: 0, millisecond: 0, microsecond: 0);
 
-    for (int i = 0; i < 7; i++) {
-      final date = now.subtract(Duration(days: i));
-      final dateString = date.toIso8601String().split('T').first;
+  //   for (int i = 0; i < 7; i++) {
+  //     final date = now.subtract(Duration(days: i));
+  //     final dateString = date.toIso8601String().split('T').first;
 
-      final steps = 10000 - (i * 500) + (i % 2 == 0 ? 300 : -200);
-      final calories = 2500 - (i * 50);
-      final sleepQuality = (0.9 - (i * 0.05)).clamp(0.4, 0.9);
+  //     final steps = 10000 - (i * 500) + (i % 2 == 0 ? 300 : -200);
+  //     final calories = 2500 - (i * 50);
+  //     final sleepQuality = (0.9 - (i * 0.05)).clamp(0.4, 0.9);
 
-      final data = MetricData(
-        date: dateString,
-        steps: steps.toInt(),
-        calories: calories,
-        sleepQuality: sleepQuality,
-      );
+  //     final data = MetricData(
+  //       date: dateString,
+  //       steps: steps.toInt(),
+  //       calories: calories,
+  //       sleepQuality: sleepQuality,
+  //     );
 
-      await db.insert(
-        tableNameMetrics,
-        data.toMap(),
-        conflictAlgorithm: ConflictAlgorithm.replace,
-      );
-    }
-  }
+  //     await db.insert(
+  //       tableNameMetrics,
+  //       data.toMap(),
+  //       conflictAlgorithm: ConflictAlgorithm.replace,
+  //     );
+  //   }
+  // }
 }
